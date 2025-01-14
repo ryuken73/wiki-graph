b502e9614f656b67363994dbf98a19720c2fa070
ryuken73 <ryuken73@gmail.com>
2025-01-14 09:25:43 +0900

    update: when node is hovered, its neighbors are highlighted

--- src/Graph2D.jsx
+++ src/Graph2D.jsx
@@ -3,7 +3,30 @@ import {ForceGraph2D} from 'react-force-graph';
 
 function Graph2D(props, graphRef) {
   const {graphData, handleNodeClick, handleLeftClick} = props;
+  const [hightlightNodes, setHighligntNodes] = React.useState(new Set());
   // const fgRef = React.useRef(null);
+
+  const updateHighlight = React.useCallback(() => {
+    setHighligntNodes(hightlightNodes)
+  }, [])
+
+  const handleNodeHover = React.useCallback((node) => {
+    setHighligntNodes((hightlightNodes) => {
+      hightlightNodes.clear();
+      if(node){
+        console.log(node, node.neighbors)
+        hightlightNodes.add(node);
+        node.neighbors.forEach(neighbor => hightlightNodes.add(neighbor));
+      }
+      return hightlightNodes
+    })
+    // hightlightNodes.clear();
+    // if(node){
+    //   hightlightNodes.add(node);
+    //   node.neighbors.forEach(neighbor => hightlightNodes.add(neighbor));
+    //   updateHighlight();
+    // }
+  }, [])
   return (
     <ForceGraph2D
       ref={graphRef}
@@ -22,12 +45,13 @@ function Graph2D(props, graphRef) {
         node.fy = node.y;
         node.fz = node.z;
       }}
+      onNodeHover={handleNodeHover}
       nodeCanvasObject={(node, ctx, globalScale) => {
         const label = node.text;
         const fontSize = 12/globalScale;
         ctx.font = `${fontSize}px Sans-Serif`;
         const textWidth = ctx.measureText(label).width;
-        const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
+        const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.5); // some padding
 
         ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
         ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
@@ -36,6 +60,14 @@ function Graph2D(props, graphRef) {
         ctx.textBaseline = 'middle';
         ctx.fillStyle = node.color;
         ctx.fillText(label, node.x, node.y);
+
+        const needHighlight = hightlightNodes.has(node);
+        if(needHighlight){
+          ctx.lineWidth = 0.5;
+          ctx.beginPath();
+          ctx.strokeRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+          ctx.strokeStyle = 'yellow';
+        }
 
         node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
       }}
